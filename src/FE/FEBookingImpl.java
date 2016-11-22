314e3dfc58f1b543e126501486ed5c77a9757fde
Sajjad Ashraf <ssajjadashraf@yahoo.com>
2016-11-21 20:53:57 -0500

    first commit

---
+++ src/FE/FEBookingImpl.java
@@ -0,0 +1,32 @@
+package FE;
+
+public class FEBookingImpl extends FEBookingIntPOA {
+
+	@Override
+	public String bookFlight(String firstName, String lastName, String address,
+			String phone, String destination, String date, String classFlight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBookedFlightCount(String recordType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String editFlightRecord(String recordID, String fieldName,
+			String newValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String transferReservation(String passengerID, String currentCity,
+			String otherCity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/FE/FEMain.java
@@ -0,0 +1,13 @@
+package FE;
+
+public class FEMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
