3a964782915f3cb9ed0bb77ad4681d32a582f82c
UlanB <usbaitass@gmail.com>
2016-11-28 16:28:45 -0500

    some modification to FE

--- src/FE/FEBookingImpl.java
+++ src/FE/FEBookingImpl.java
@@ -1,33 +1,55 @@
 package FE;
 
+import org.omg.CORBA.ORB;
+
 public class FEBookingImpl extends FEBookingIntPOA {
+	
+	private ORB orb;
+	
+	public FEBookingImpl(){}
 
 	@Override
 	public String bookFlight(String firstName, String lastName, String address,
 			String phone, String destination, String date, String classFlight) {
 		// TODO Auto-generated method stub
-		System.out.println("Good luck!");
-		return null;
+		System.out.println("inside bookFlight");
+		return "";
 	}
 
 	@Override
 	public String getBookedFlightCount(String recordType) {
 		// TODO Auto-generated method stub
-		return null;
+		System.out.println("inside getBookedFlightCount");
+		return "";
 	}
 
 	@Override
-	public String editFlightRecord(String recordID, String fieldName,
-			String newValue) {
+	public String editFlightRecord(String recordID, String fieldName, String newValue) {
 		// TODO Auto-generated method stub
-		return null;
+		System.out.println("inside editFlightRecord");
+		return "";
 	}
 
 	@Override
-	public String transferReservation(String passengerID, String currentCity,
-			String otherCity) {
+	public String transferReservation(String passengerID, String currentCity, String otherCity) {
 		// TODO Auto-generated method stub
-		return null;
+		System.out.println("inside transferReservation");
+		return "";
+	}
+	
+	/**
+	 * This method sets the orb
+	 * @param new_orb
+	 */
+	public void setORB(ORB new_orb){
+		orb = new_orb;
+	}
+	
+	/**
+	 * This method shut downs the orb
+	 */
+	public void shutdown(){
+		orb.shutdown(false);
 	}
 
 }
--- src/FE/FEMain.java
+++ src/FE/FEMain.java
@@ -1,15 +1,77 @@
 package FE;
 
-public class FEMain {
+//import java.net.DatagramSocket;
+//import java.net.SocketException;
 
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.InvalidName;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.omg.PortableServer.POAPackage.ServantNotActive;
+import org.omg.PortableServer.POAPackage.WrongPolicy;
+
+public class FEMain {
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		// whats up
-		System.out.println("Hello world!");
+		System.out.println("Front End has started.");
+	//	int portUDP = 5555;
+		
+		try {
+			//CORBA configuration
+			ORB orb = ORB.init(args, null);
+			POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			rootPOA.the_POAManager().activate();
+			FEBookingImpl bookingObject = new FEBookingImpl();
+			bookingObject.setORB(orb);
+			org.omg.CORBA.Object ref = rootPOA.servant_to_reference(bookingObject);
+			FEBookingInt href = FEBookingIntHelper.narrow(ref);
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			NameComponent path[] = ncRef.to_name("FRONTEND");
+			ncRef.rebind(path, href);
+			
+			orb.run();
+			
+	//		DatagramSocket socket = new DatagramSocket(portUDP);
+	//		socket.close();
+			
+	//	} catch (SocketException e) {
+	//		e.printStackTrace();
+	//		System.err.println("datagram socket exception");
+		} catch (NotFound e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotProceed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidName e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.omg.CORBA.ORBPackage.InvalidName e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServantNotActive e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WrongPolicy e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AdapterInactive e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		
 	}
