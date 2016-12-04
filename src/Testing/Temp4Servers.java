2ca51d25108ea5ab12900cc86392480788651543
Sajjad Ashraf <ssajjadashraf@yahoo.com>
2016-12-03 19:08:22 -0500

    Sequencer Change.

--- src/Testing/Temp4Servers.java
+++ src/Testing/Temp4Servers.java
@@ -16,14 +16,14 @@ public class Temp4Servers {
 
 	public static void main(String[] args) {
 
-		send(StaticContent.REPLICA_ULAN_IP_ADDRESS, StaticContent.REPLICA_ULAN_lISTENING_PORT, "Ulan");
-		send(StaticContent.REPLICA_SAJJAD_IP_ADDRESS, StaticContent.REPLICA_SAJJAD_lISTENING_PORT, "Sajjad");
-		send(StaticContent.REPLICA_UMER_IP_ADDRESS, StaticContent.REPLICA_UMER_lISTENING_PORT, "Ulan");
-		send(StaticContent.REPLICA_FERAS_IP_ADDRESS, StaticContent.REPLICA_FERAS_lISTENING_PORT, "Ulan");
+		send(StaticContent.REPLICA_ULAN_IP_ADDRESS, StaticContent.REPLICA_ULAN_lISTENING_PORT, StaticContent.SEQUENCER_ACK_PORT_FOR_REPLICA_ULAN, "Ulan");
+		send(StaticContent.REPLICA_SAJJAD_IP_ADDRESS, StaticContent.REPLICA_SAJJAD_lISTENING_PORT, StaticContent.SEQUENCER_ACK_PORT_FOR_REPLICA_SAJJAD, "Sajjad");
+		send(StaticContent.REPLICA_UMER_IP_ADDRESS, StaticContent.REPLICA_UMER_lISTENING_PORT, StaticContent.SEQUENCER_ACK_PORT_FOR_REPLICA_UMER,"Ulan");
+		send(StaticContent.REPLICA_FERAS_IP_ADDRESS, StaticContent.REPLICA_FERAS_lISTENING_PORT, StaticContent.SEQUENCER_ACK_PORT_FOR_REPLICA_FERAS, "Ulan");
 
 	}
 
-	public static void send(final String ip_address, final int port, final String new_ans) {
+	public static void send(final String ip_address, final int port, final int acknowledgementPort, final String new_ans) {
 		System.out.println("server is up: "+ ip_address+" , port: "+ port);
 			
 			
@@ -31,7 +31,7 @@ public class Temp4Servers {
 				@Override
 				public void run() {
 						
-						Reciever r = new Reciever(port, StaticContent.SEQUENCER_ACK_PORT);
+						Reciever r = new Reciever(port, acknowledgementPort);
 
 						System.out.println("the data received is : "
 								+ r.getData().getServerName());
